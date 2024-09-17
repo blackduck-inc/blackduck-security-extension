7c9e53664b3661039d2288b40c2860a7f0c0be03
lokesh <lokesha@synopsys.com>
2024-09-17 21:59:33 +0530

    SIGINT-2132 refactor

---
+++ blackduck-security-task/src/blackduck-security-task/diagnostics.ts
@@ -0,0 +1,47 @@
+// Copyright (c) 2024 Black Duck Software Inc. All rights reserved worldwide.
+
+import * as taskLib from "azure-pipelines-task-lib/task";
+import * as constants from "./application-constant";
+import * as path from "path";
+import { getDefaultSarifReportPath } from "./utility";
+
+export function uploadDiagnostics(workspaceDir: string) {
+  const uploadArtifactPath = path.join(
+    workspaceDir,
+    constants.BRIDGE_CLI_LOCAL_DIRECTORY
+  );
+  let isBridgeDirectoryExists = false;
+  isBridgeDirectoryExists = taskLib.exist(uploadArtifactPath);
+  if (isBridgeDirectoryExists) {
+    taskLib.uploadArtifact(
+      constants.UPLOAD_FOLDER_ARTIFACT_NAME,
+      uploadArtifactPath,
+      constants.UPLOAD_FOLDER_ARTIFACT_NAME
+    );
+  }
+}
+
+export function uploadSarifResultAsArtifact(
+  defaultSarifReportDirectory: string,
+  userSarifFilePath: string
+) {
+  const sarifFilePath = userSarifFilePath
+    ? userSarifFilePath
+    : getDefaultSarifReportPath(defaultSarifReportDirectory, true);
+
+  let isSarifReportDirectoryExists = false;
+  isSarifReportDirectoryExists = taskLib.exist(sarifFilePath);
+  if (isSarifReportDirectoryExists) {
+    console.log(`Uploading SARIF report as artifact from: ${sarifFilePath}`);
+    taskLib.uploadArtifact(
+      constants.SARIF_UPLOAD_FOLDER_ARTIFACT_NAME,
+      sarifFilePath,
+      constants.SARIF_UPLOAD_FOLDER_ARTIFACT_NAME
+    );
+    console.log("Upload SARIF report successfully in the artifact");
+  } else {
+    console.log(
+      `Uploading SARIF report as artifact failed as file path not found at: ${sarifFilePath}`
+    );
+  }
+}
