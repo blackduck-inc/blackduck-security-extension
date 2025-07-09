b9bab8f9faf03446d077f5d586f06534b04bf284
kirann <kirann@blackduck.com>
2025-07-09 20:47:38 +0530

    feat(SIGINT-3253): Support Azure DevOps Server-implement dynamic API version fetching for Azure service

--- blackduck-security-task/src/blackduck-security-task/azure-service-client.ts
+++ blackduck-security-task/src/blackduck-security-task/azure-service-client.ts
@@ -1,5 +1,6 @@
 // Copyright (c) 2024 Black Duck Software Inc. All rights reserved worldwide.
 
+import { HttpClient } from "typed-rest-client/HttpClient";
 import { AzureData, AzurePrResponse } from "./model/azure";
 import * as taskLib from "azure-pipelines-task-lib/task";
 import * as constants from "./application-constant";
@@ -13,10 +14,12 @@ import { getSharedHttpClient } from "./utility";
 export class AzureService {
   azureGetMergeRequestsAPI: string;
   apiVersion: string;
+  azureGetRepositoryAPI: string;
 
   constructor() {
     this.azureGetMergeRequestsAPI =
       "/{0}/{1}/_apis/git/repositories/{2}/pullrequests?searchCriteria.status=active&$top=1&searchCriteria.sourceRefName={3}&api-version={4}";
+    this.azureGetRepositoryAPI = "/{0}/{1}/_apis/git/repositories/{2}";
     this.apiVersion = "7.0";
   }
 
@@ -33,6 +36,17 @@ export class AzureService {
           /{(\d+)}/g,
           (match, index) => encodeURIComponent(args[index]) || ""
         );
+
+      const httpClient = new HttpClient("blackduck-azure-service");
+      const urlObj = new URL(azureData.api.url);
+      if (urlObj.hostname !== "dev.azure.com") {
+        this.apiVersion = await this.fetchAzureServerApiVersion({
+          httpClient,
+          azureData,
+          StringFormat,
+        });
+        taskLib.debug(`API Version of Azure instance: ${this.apiVersion}`);
+      }
 
       const endpoint = StringFormat(
         azureData.api.url.concat(this.azureGetMergeRequestsAPI),
@@ -80,5 +94,50 @@ export class AzureService {
       }
     }
     return undefined;
+  }
+
+  private async fetchAzureServerApiVersion({
+    httpClient,
+    azureData,
+    StringFormat,
+  }: {
+    httpClient: HttpClient;
+    azureData: AzureData;
+    StringFormat: (url: string, ...args: string[]) => string;
+  }): Promise<string> {
+    const repoEndpoint = StringFormat(
+      azureData.api.url.concat(this.azureGetRepositoryAPI),
+      azureData.organization.name,
+      azureData.project.name,
+      azureData.repository.name
+    );
+    taskLib.debug(`Fetching Azure server API version from: ${repoEndpoint}`);
+    const token: string = ":".concat(azureData.user.token);
+    const encodedToken: string = Buffer.from(token, "utf8").toString("base64");
+    return await this.getVersionForAzureServer(
+      httpClient,
+      repoEndpoint,
+      encodedToken
+    );
+  }
+
+  private async getVersionForAzureServer(
+    httpClient: HttpClient,
+    repoEndpoint: string,
+    encodedToken: string
+  ) {
+    const response = await httpClient.get(repoEndpoint, {
+      Authorization: "Basic ".concat(encodedToken),
+      Accept: "application/json",
+    });
+    const header =
+      response.message.headers["content-type"] ||
+      response.message.headers["Content-Type"];
+    const match =
+      typeof header === "string" && header.match(/api-version=([\d.]+)/);
+    if (match) {
+      return match[1];
+    }
+    return "";
   }
 }
